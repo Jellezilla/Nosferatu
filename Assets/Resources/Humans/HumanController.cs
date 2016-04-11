0f2759e9bc09f15b41de4400e83d6b3ec37f8f5c
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-04-11 15:33:04 +0200

    ++ HUMANS FIXEEEDDDDDD HORRAY BAGEL!

--- Assets/Resources/Humans/HumanController.cs
+++ Assets/Resources/Humans/HumanController.cs
@@ -23,7 +23,7 @@ public class HumanController : MonoBehaviour {
 
    
     // Use this for initialization
-    void Start () {
+    void Awake () {
         Init();
         
 	}
@@ -41,13 +41,23 @@ public class HumanController : MonoBehaviour {
 
         for (int i = 0; i < m_rbs.Length; i++)
         {
-            m_cols[i].enabled = false;
-            m_rbs[i].useGravity = false;
-            m_rbs[i].Sleep();
-        }
+            if (i != 0)
+            {
+                if (i == 1)
+                {
+                    m_cols[i].enabled = true;
+                }
+                else
+                {
+                    m_cols[i].enabled = false;
+                }
 
-        m_cols[0].enabled = true;
-      //  player = GameObject.FindWithTag("Player");
+                m_rbs[i].useGravity = false;
+                m_rbs[i].isKinematic = true;
+                m_rbs[i].Sleep();
+            }
+
+        }
         
    
     }
@@ -82,21 +92,32 @@ public class HumanController : MonoBehaviour {
             //  theCol.enabled = false;
             for (int i = 0; i < m_rbs.Length; i++)
             {
-                if (i == 0)
+                if (i != 0)
+                {
+                    if (i == 1)
+                    {
+                        m_cols[i].enabled = false;
+                        //acount for root object collider
+                    }
+                    else
+                    {
+                        m_cols[i].enabled = true;
+                    }
+                    m_rbs[i].useGravity = true;
+                    m_rbs[i].isKinematic = false;
+                    m_rbs[i].WakeUp();
+                }
+                else //if its equal to 0
                 {
                     m_cols[i].enabled = false;
-                }
-                else
-                {
-                    m_cols[i].enabled = true;
+                    m_rbs[i].useGravity = false;
+                    m_rbs[i].Sleep();
                 }
 
-                m_rbs[i].useGravity = true;
-                m_rbs[i].Sleep();
+
             }
-
-            _isDead = true;
             GoRagdoll();
+            _isDead = true;
         }
     }
 
