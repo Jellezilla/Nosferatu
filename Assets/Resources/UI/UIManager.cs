454deb7eaed1fdafeb482416871645815be9d9e9
Peter Vestergaard Jørgensen <pvjoergensen@hotmail.com>
2016-04-16 15:59:48 +0200

    ++ UI functionality; blood/fuel depletes, kills gains souls + fuel

--- Assets/Resources/UI/UIManager.cs
+++ Assets/Resources/UI/UIManager.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject Healthbar;
+    public GameObject Soulsbar;
+    private RectTransform _hpmask;
+    private RectTransform _spmask;
+    private float _spMaxWidth, _hpMaxWidth, _spHeight,_hpHeight;
+
+    void Start () {
+        _hpmask = Healthbar.transform.GetChild(0).gameObject.GetComponent<RectTransform>();
+        _spmask = Soulsbar.transform.GetChild(0).gameObject.GetComponent<RectTransform>();
+
+        _spMaxWidth = _spmask.GetComponent<RectTransform>().rect.width;
+        _hpMaxWidth = _hpmask.GetComponent<RectTransform>().rect.width;
+        _spHeight = _spmask.GetComponent<RectTransform>().rect.height;
+        _hpHeight = _hpmask.GetComponent<RectTransform>().rect.height;
+
+        Debug.Log(_hpHeight);
+    }
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
-	}
+        float fuelCurrent = GameController.Instance.GetFuel;
+        float soulsCurrent = GameController.Instance.GetSouls;
+        Debug.Log(soulsCurrent);
+        _hpmask.sizeDelta = new Vector2(_hpMaxWidth/100 * fuelCurrent, _hpHeight);// _hpHeight);
+        _spmask.sizeDelta = new Vector2(_spMaxWidth/100 * (1+soulsCurrent), _spHeight);// _hpHeight);
+    }
+
 }
