ecbc559d5a07f075524e1a3f7399569f87217dc4
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-04-27 12:56:16 +0200

    ++ structure ++fixes ++ tile_setup

---
+++ Assets/Resources/GameManager/Scripts/TileManager.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+    private Vector3 m_playerSpawnPosition;
+
+}
---
+++ Assets/Resources/Tile prefabs/Scripts/Tile.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    private bool m_initTile;
+    [SerializeField]
+    private GameObject m_Humans;
+    [SerializeField]
+    private GameObject m_tombstones;
+    // Use this for initialization
+
+    void OnEnable()
+    {
+        if (!m_initTile)
+        {
+
+        }
+    }
+    
+    void Awake ()
+    {
+        if (!m_initTile)
+        {
+
+        }
+
+	}
+
+    void InitTile()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
