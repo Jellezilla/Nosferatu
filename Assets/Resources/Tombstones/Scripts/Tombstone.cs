f07a1d0330d76c8d8acdaac93a9917ce0ab8c5ef
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-04-28 00:33:31 +0200

    ++ Tombstones stage 2

--- Assets/Resources/Tombstones/Scripts/Tombstone.cs
+++ Assets/Resources/Tombstones/Scripts/Tombstone.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Tombstone : MonoBehaviour {
 
     private Rigidbody[] m_childrenRbs;
-    private BoxCollider[] m_childrenCols;
+    private Vector3[] m_childPositions;
+    private Quaternion[] m_childRotations;
+    private List<BoxCollider> m_childrenCols;
+    private BoxCollider m_col;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+
+        Init();
 	
 	}
 	
@@ -17,16 +22,60 @@ public class Tombstone : MonoBehaviour {
 
     void Init()
     {
-        for (int i = 0; i < gameObject.transform.childCount; i++)
+        m_childrenRbs = new Rigidbody[transform.childCount];
+        m_childPositions = new Vector3[transform.childCount];
+        m_childRotations = new Quaternion[transform.childCount];
+        m_childrenCols = new List<BoxCollider>();
+        m_col = GetComponent<BoxCollider>();
+        for (int i = 0; i < transform.childCount; i++)
         {
-
+            m_childrenRbs[i] = transform.GetChild(i).GetComponent<Rigidbody>();
+            m_childPositions[i] = m_childrenRbs[i].transform.position;
+            m_childRotations[i] = m_childrenRbs[i].transform.rotation;
+            BoxCollider[] boxCol = m_childrenRbs[i].GetComponents<BoxCollider>();
+            for (int j = 0; j < boxCol.Length; j++)
+            {
+                m_childrenCols.Add(boxCol[j]);
+            }
         }
     }
+
+    void Reset()
+    {
+
+
+        for (int i = 0; i < m_childrenRbs.Length; i++)
+        {
+            m_childrenRbs[i].isKinematic = true;
+
+        }
+
+        for (int i = 0; i < m_childrenCols.Count; i++)
+        {
+            m_childrenCols[i].enabled = false;
+        }
+
+        m_col.enabled = true;
+    }
+
+
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("hitting tombstone");
         if (collision.gameObject.tag == Tags.player || collision.gameObject.tag == Tags.human)
         {
+            m_col.enabled = false;
+
+            for (int i = 0; i < m_childrenCols.Count; i++)
+            {
+                m_childrenCols[i].enabled = true;
+            }
+
+            for (int i = 0; i < m_childrenRbs.Length; i++)
+            {
+                m_childrenRbs[i].isKinematic = false;
+
+            }
+
 
         }
     }
