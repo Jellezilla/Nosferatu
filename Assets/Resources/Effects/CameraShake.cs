a7c1897d265884db1e389f8830b90b3b73820bd2
Peter Vestergaard Jørgensen <pvjoergensen@hotmail.com>
2016-05-11 02:28:17 +0200

    ++ Stone slab floors and ugly billboard grass for detailing tiles, also stupid camera effect

---
+++ Assets/Resources/Effects/CameraShake.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+ using System.Collections;
+ 
+ public class CameraShake : MonoBehaviour {
+
+    private Vector3 originPosition;
+    private Quaternion originRotation;
+    public float shake_decay;
+    public float shake_intensity;
+
+    void Awake(){
+        ShakeCamera();
+    }
+    void Update() {     
+        
+        if (shake_intensity > 0) {
+            transform.position = originPosition + Random.insideUnitSphere * shake_intensity;
+            transform.rotation = new Quaternion(
+                originRotation.x + Random.Range(-shake_intensity, shake_intensity) * .02f,
+                originRotation.y + Random.Range(-shake_intensity, shake_intensity) * .02f,
+                originRotation.z + Random.Range(-shake_intensity, shake_intensity) * .02f,
+                originRotation.w + Random.Range(-shake_intensity, shake_intensity) * .02f);
+            shake_intensity -= shake_decay;
+        }
+    }
+
+    public void ShakeCamera() {
+        originPosition = transform.position;
+        originRotation = transform.rotation;
+
+    }
+}
