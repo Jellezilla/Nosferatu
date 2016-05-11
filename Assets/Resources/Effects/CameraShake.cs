731b176d1a8c67de73f42aa44fab64f311f1894f
Peter Vestergaard Jørgensen <pvjoergensen@hotmail.com>
2016-05-11 16:46:17 +0200

    ++ Added Local Highscore, functionality + graphics

--- Assets/Resources/Effects/CameraShake.cs
+++ Assets/Resources/Effects/CameraShake.cs
@@ -5,28 +5,34 @@
 
     private Vector3 originPosition;
     private Quaternion originRotation;
+    private Quaternion standardRotation;
     public float shake_decay;
     public float shake_intensity;
 
-    void Awake(){
+    void OnEnable(){
+        standardRotation = transform.rotation;
         ShakeCamera();
     }
-    void Update() {     
-        
+
+    void Update() {
+        ShakeCamera();
         if (shake_intensity > 0) {
-            transform.position = originPosition + Random.insideUnitSphere * shake_intensity;
-            transform.rotation = new Quaternion(
+            transform.localPosition = originPosition + Random.insideUnitSphere * shake_intensity;
+            transform.localRotation = new Quaternion(
                 originRotation.x + Random.Range(-shake_intensity, shake_intensity) * .02f,
                 originRotation.y + Random.Range(-shake_intensity, shake_intensity) * .02f,
                 originRotation.z + Random.Range(-shake_intensity, shake_intensity) * .02f,
                 originRotation.w + Random.Range(-shake_intensity, shake_intensity) * .02f);
             shake_intensity -= shake_decay;
         }
+        else  {
+            transform.rotation = standardRotation;
+            Debug.Log("end");
+        }
     }
 
     public void ShakeCamera() {
         originPosition = transform.position;
         originRotation = transform.rotation;
-
     }
 }
